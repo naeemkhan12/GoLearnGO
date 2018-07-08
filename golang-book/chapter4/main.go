a0b43c3e2f3f95fb8fd8adf696d22681db9a8a2a
naeemkhan12 <naeemb7070@gmail.com>
2018-07-08 19:16:42 +0500

    add chapter 5

--- golang-book/chapter4/main.go
+++ golang-book/chapter4/main.go
@@ -2,9 +2,22 @@
  import "fmt"
  func main() {
 	 /*Go variables*/
+	 // with type declaration
 	 var x string;
 	 var y string="John";
+	 //without type declaration
+	 z :="Glad to See ya !!";
 	 x="Hello ";
-	 fmt.Println(x,y);
+	 // constants
+	 const constant string="I am a constant"
+	 // declaring multiple variables
+	 var (
+		 a=12
+		 b=23
+		 c="Stringing"
+	 )
+	 fmt.Println(x,y,z);
+	 fmt.Println(constant);
+	 fmt.Println(a,b,c)
 
  }

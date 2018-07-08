6a65bd0a297dc7c6718311b598b6e0ffca54d7b9
naeemkhan12 <naeemb7070@gmail.com>
2018-07-08 12:41:59 +0500

    go print statement and variables

---
+++ golang-book/chapter3/main.go
@@ -0,0 +1,9 @@
+ package main
+ import "fmt"
+ func main() {
+	 fmt.Println("3+4",3+4)
+	 fmt.Println("Hello World"[1])
+	 fmt.Println("Hello"+"World")
+	 fmt.Println(true && false)
+
+ }
---
+++ golang-book/chapter4/main.go
@@ -0,0 +1,10 @@
+ package main
+ import "fmt"
+ func main() {
+	 /*Go variables*/
+	 var x string;
+	 var y string="John";
+	 x="Hello ";
+	 fmt.Println(x,y);
+
+ }
